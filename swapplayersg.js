489a131b3809faf909fe53b3f2ffd3e86bbc053f
21341414 <phamj@pdxeaglestudent.org>
2026-01-29 11:05:17 -0800

    Create swapplayersg.js

---
+++ swapplayersg.js
@@ -0,0 +1,38 @@
+(() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    
+    /* By CryptoDude3 */
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    
+    const cheat = (async () => {
+        let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { 
+            return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) 
+        })())[1].children[0]._owner;
+        const player = prompt("Swap With? (Case sensitive)");
+        stateNode.props.liveGameController.getDatabaseVal("c", (players) => {
+            if (!players || players[player] == null) return;
+            const gold = players[player].g || 0;
+            stateNode.props.liveGameController.setVal({ 
+                path: "c/" + stateNode.props.client.name, 
+                val: { 
+                    b: stateNode.props.client.blook, 
+                    tat: player + ":swap:" + (stateNode.state.gold || 0), 
+                    g: gold 
+                } 
+            });
+            stateNode.setState({ gold, gold2: gold });
+        });
+    });
+    
+    cheat();
+})();
