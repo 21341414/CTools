4cbaf34816c25ba9908bf3c43357df2b775c5d87
21341414 <phamj@pdxeaglestudent.org>
2026-01-29 11:05:39 -0800

    Create resetplayersg.js

---
+++ resetplayersg.js
@@ -0,0 +1,32 @@
+(() => {
+    let iframe = document.querySelector("iframe");
+    if (!iframe) {
+        iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        document.body.append(iframe);
+    }
+    
+    /* By CryptoDude3 */
+    if (window.fetch.call.toString() == 'function call() { [native code] }') {
+        const call = window.fetch.call;
+        window.fetch.call = function () {
+            if (!arguments[1].includes("s.blooket.com/rc")) return call.apply(this, arguments);
+        }
+    }
+    
+    const cheat = (async () => {
+        let i = document.createElement('iframe');
+        document.body.append(i);
+        window.prompt = i.contentWindow.prompt.bind(window);
+        i.remove();
+        let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { 
+            return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) 
+        })())[1].children[0]._owner;
+        stateNode.props.liveGameController.setVal({ 
+            path: "c/" + stateNode.props.client.name + "/tat", 
+            val: prompt("Reset Who's Gold") + ":swap:0" 
+        });
+    });
+    
+    cheat();
+})();
