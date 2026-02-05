15464f94bf2dd0a5afa814c7967652f0a32ef11c
21341414 <phamj@pdxeaglestudent.org>
2026-02-05 10:23:44 -0800

    Update tiktokapp.sh

--- applications/tiktokapp.sh
+++ applications/tiktokapp.sh
@@ -1,19 +1,23 @@
-sudo dpkg --configure -a && \
-sudo apt update && sudo apt install -y nodejs npm chromium && \
+rm -rf ~/TikTok-linux-x64 && \
+rm -f ~/.local/share/applications/tiktok.desktop && \
+update-desktop-database ~/.local/share/applications && \
+sudo dpkg --configure -a && sudo apt update && sudo apt install -y nodejs npm chromium && \
 sudo npm install -g nativefier && \
 nativefier --name TikTok --single-instance --disable-dev-tools --no-sandbox https://www.tiktok.com && \
+mkdir -p ~/TikTok-linux-x64 && \
+wget -O ~/TikTok-linux-x64/tiktok.png https://raw.githubusercontent.com/21341414/CTools/refs/heads/main/applications/tiktok.png && \
 mkdir -p ~/.local/share/applications && \
 cat > ~/.local/share/applications/tiktok.desktop << 'EOF'
 [Desktop Entry]
 Name=TikTok
 Comment=TikTok Web Wrapper
 Exec=/home/linux/TikTok-linux-x64/TikTok --disable-gpu
-Icon=/home/linux/TikTok-linux-x64/resources/app/icon.png
+Icon=/home/linux/TikTok-linux-x64/tiktok.png
 Terminal=false
 Type=Application
 Categories=Network;Video;
 StartupWMClass=TikTok
 EOF
 update-desktop-database ~/.local/share/applications && \
-echo "TikTok Wrapper Installed! Launching..\n Thank KHXGVNG" && \
+echo -e "TikTok Wrapper Installed! Launching..\nThank KHXGVNG" && \
 ~/TikTok-linux-x64/TikTok --disable-gpu
